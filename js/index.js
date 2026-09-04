f1714766bc3f6f8effe1ca0a0128edce6f910648
Fred <frederikpriesshansen@gmail.com>
2026-09-04 09:24:10 +0200

    tilføjet hero section fra js til html

--- js/index.js
+++ js/index.js
@@ -1,5 +1,48 @@
 // slå dig løs her... 
-import * as data from './data.js'
+import * as Data from './data.js'
+
+/* Hero section */
+const heroSection = document.querySelector(".hero")
+
+/* hero container */
+const heroContainer = document.createElement('div')
+heroContainer.classList.add('hero-container')
+
+/* Hero baggrund */
+const heroImage = document.createElement('img')
+heroImage.classList.add('hero-background')
+heroImage.src = Data.hero.image
+heroImage.alt = 'Hero Background'
+
+/* hero text */
+const heroCard = document.createElement('div')
+heroCard.classList.add('hero-card')
+
+const heroHeadline = document.createElement('h1')
+heroHeadline.textContent = Data.hero.headline
+
+const heroCopy = document.createElement('p')
+heroCopy.textContent = Data.hero.copy
+ /* button */
+const heroButton = document.createElement('a')
+heroButton.classList.add('hero-button')
+heroButton.href = '#'
+
+const buttonIcon = document.createElement('img');
+buttonIcon.src = Data.hero.icon;
+buttonIcon.alt = 'Globe icon';
+
+const buttonText = document.createElement('span')
+buttonText.textContent = 'Explore'
+
+/* append hero container, card, image og button */
+heroButton.append(buttonIcon, buttonText)
+heroCard.append(heroHeadline, heroCopy, heroButton)
+heroContainer.append(heroImage, heroCard)
+heroSection.append(heroContainer)
+
+
+/* service section */
 
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
