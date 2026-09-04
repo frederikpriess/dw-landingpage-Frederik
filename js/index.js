761e0ecec178814d1134b6bd04ecd6f33cd068f7
Fred <frederikpriesshansen@gmail.com>
2026-09-04 08:53:04 +0200

    tilføjet "module" type til indes.js link i html tilføjet export til alle objekter i data.js tilføjet import af alle objekterne så de alle ligger under "data"

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // slå dig løs her... 
+import * as data from './data.js'
+
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+/* services.forEach(service => console.log(service.headline)) */
